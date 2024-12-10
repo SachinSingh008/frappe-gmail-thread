8ead5f250a55160c9ec073e56bc81ce5c175cc6a
Shivam Saini <shivamsn97@gmail.com>
2024-12-10 04:59:46 +0000

    fix: Show activity on all doctypes, and fix gravatar

--- frappe_gmail_thread/public/js/activity.js
+++ frappe_gmail_thread/public/js/activity.js
@@ -1,5 +1,4 @@
 const setup_gmail_threads_activity = function(frm) {
-    alert("hello.")
     // get linked email thread
     const doctype = frm.doctype;
     const docname = frm.docname;
@@ -21,17 +20,62 @@ const setup_gmail_threads_activity = function(frm) {
 }
 
 // call before_load function when form is loaded but activity is not yet loaded
-$(document).on("load", function (event, frm) {
+$(document).on("form-refresh", function(event, frm) {
     frappe.ui.form.on(frm.doctype, {
-        before_load: setup_gmail_threads_activity
+        refresh: setup_gmail_threads_activity
     });
 });
 
 function add_gmail_threads_to_form(frm, gmail_threads) {
-    // add gmail threads to the form
-    const timeline_communications = frm.timeline.doc_info.communications
-    for (let gmail_thread of gmail_threads) {
-        // push gmail thread to timeline communications
-        timeline_communications.push(gmail_thread);
+    if (!gmail_threads || gmail_threads.length === 0) {
+        return;
     }
+    // add gmail threads to the form
+    const timeline = frm.timeline
+    timeline.refresh();
+    const communication_contents = [];
+    const all_emails = [];
+    for (let gmail_thread of gmail_threads) {
+        all_emails.push(gmail_thread.sender);
+    }
+    const unique_emails = [...new Set(all_emails)];
+    // fetch user info from http://localhost/api/method/frappe.desk.form.load.get_user_info_for_viewers?users=[%22shivam.saini@rtcamp.com%22]
+    frappe.call({
+        method: "frappe.desk.form.load.get_user_info_for_viewers",
+        args: {
+            users: JSON.stringify(unique_emails)
+        },
+        callback: function(response) {
+            const user_info = response.message;
+            frappe.update_user_info(user_info);
+            for (let gmail_thread of gmail_threads) {
+                const email = gmail_thread.sender;
+                const communication_content = get_gthread_timeline_content(gmail_thread);
+                const t_data = {
+                    icon: "mail",
+                    icon_size: "sm",
+                    creation: gmail_thread.creation,
+                    is_card: true,
+                    content: communication_content,
+                    doctype: "Gmail Thread",
+                    id: `gmail-thread-${gmail_thread.name}`,
+                    name: gmail_thread.name,
+                }
+                communication_contents.push(t_data);
+            }
+            timeline.add_timeline_items_based_on_creation(communication_contents);
+        }
+    });
+}
+
+function get_gthread_timeline_content(doc, allow_reply = true) {
+    doc._url = frappe.utils.get_form_link("Gmail Thread", doc.name);
+    if (doc.attachments && typeof doc.attachments === "string") {
+        doc.attachments = JSON.parse(doc.attachments);
+    }
+    doc.owner = doc.sender;
+    doc.user_full_name = doc.sender_full_name;
+    doc.content = frappe.dom.remove_script_and_style(doc.content);
+    let communication_content = $(frappe.render_template("timeline_message_box", { doc }));
+    return communication_content;
 }
