d9e301225de9eac716dc31aea08241c6dbc5d0a1
Shivam Saini <shivamsn97@gmail.com>
2025-02-26 04:45:07 +0000

    Skip webhook calls if Sync in Realtime is disabled.

--- frappe_gmail_thread/api/pubsub.py
+++ frappe_gmail_thread/api/pubsub.py
@@ -10,6 +10,13 @@ def callback():
     data = frappe.request.get_data(as_text=True)
     data = frappe.parse_json(data)
     message = data.get("message", {}).get("data")
+    google_settings = frappe.get_single("Google Settings")
+    if not google_settings.enable:
+        return "OK"
+    if not google_settings.custom_gmail_sync_in_realtime:
+        return "OK"
+    if not google_settings.custom_gmail_pubsub_topic:
+        return "OK"
     if message:
         message = b64.b64decode(message).decode("utf-8")
         try:
